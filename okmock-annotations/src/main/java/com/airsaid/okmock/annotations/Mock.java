8f5fd0c54742b5354ca20a2e2286e4ed44265be1
Airsaid <airsaid1024@gmail.com>
2021-07-27 17:58:59 +0800

    Add okmock-annotations module

---
+++ okmock-annotations/src/main/java/com/airsaid/okmock/annotations/Mock.java
@@ -0,0 +1,15 @@
+package com.airsaid.okmock.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author airsaid
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Mock {
+
+}
