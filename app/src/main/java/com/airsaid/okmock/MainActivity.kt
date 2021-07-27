89633d8c251d88e9dc7daff1c2020dd15b519457
Airsaid <airsaid1024@gmail.com>
2021-07-27 19:00:35 +0800

    Add mock test data

--- app/src/main/java/com/airsaid/okmock/MainActivity.kt
+++ app/src/main/java/com/airsaid/okmock/MainActivity.kt
@@ -2,10 +2,54 @@ package com.airsaid.okmock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airsaid.okmock.annotations.Mock
 
 class MainActivity : AppCompatActivity() {
+
+  // 基本类型
+  @Mock
+  private var mMockBoolean: Boolean = false
+  @Mock
+  private var mMockByte: Byte = Byte.MIN_VALUE
+  @Mock
+  private var mMockShort: Short = Short.MIN_VALUE
+  @Mock
+  private var mMockInt: Int = Int.MIN_VALUE
+  @Mock
+  private var mMockLong: Long = Long.MIN_VALUE
+  @Mock
+  private var mMockFloat: Float = Float.MIN_VALUE
+  @Mock
+  private var mMockDouble: Double = Double.MIN_VALUE
+  @Mock
+  private var mMockChar: Char = Char.MIN_VALUE
+
+  // 引用类型
+  @Mock
+  private lateinit var mMockString: String
+  @Mock
+  private lateinit var mMockPerson: Person
+
+  // 引用集合
+  @Mock
+  private lateinit var mMockStrings: List<String>
+  @Mock
+  private lateinit var mMockPersons: Set<Person>
+  @Mock
+  private lateinit var mMockPersonMap: Map<Int, Person>
+
+  // 嵌套
+  @Mock
+  private lateinit var mMockParents: List<Parent>
+  @Mock
+  private lateinit var mMockParentMap: Map<Int, List<Parent>>
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
+
+  data class Person(val name: String, val age: Int)
+
+  data class Parent(val name: String, val age: Int, val children: List<Person>)
 }
