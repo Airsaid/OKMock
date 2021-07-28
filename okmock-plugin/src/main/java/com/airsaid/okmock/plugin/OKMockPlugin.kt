5001a090f6b58dce2030c3d6bc059d0556fc91e3
Airsaid <airsaid1024@gmail.com>
2021-07-28 18:16:30 +0800

    Add okmock-plugin module

---
+++ okmock-plugin/src/main/java/com/airsaid/okmock/plugin/OKMockPlugin.kt
@@ -0,0 +1,13 @@
+package com.airsaid.okmock.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author airsaid
+ */
+class OKMockPlugin : Plugin<Project> {
+  override fun apply(target: Project) {
+
+  }
+}
