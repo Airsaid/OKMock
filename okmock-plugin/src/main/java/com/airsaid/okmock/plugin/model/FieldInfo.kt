c61851c36e3f1f76034ffee6c88193b628cfa734
Airsaid <airsaid1024@gmail.com>
2021-07-29 14:27:17 +0800

    Add annotation field info handler

---
+++ okmock-plugin/src/main/java/com/airsaid/okmock/plugin/model/FieldInfo.kt
@@ -0,0 +1,6 @@
+package com.airsaid.okmock.plugin.model
+
+/**
+ * @author airsaid
+ */
+data class FieldInfo(val access: Int, val name: String?, val descriptor: String?, val signature: String?, val value: Any?)
--- okmock-plugin/src/main/java/com/airsaid/okmock/plugin/transform/OKMockTransform.kt
+++ okmock-plugin/src/main/java/com/airsaid/okmock/plugin/transform/OKMockTransform.kt
@@ -1,12 +1,17 @@
 package com.airsaid.okmock.plugin.transform
 
+import com.airsaid.okmock.api.Mock
 import com.airsaid.okmock.plugin.OKMockPlugin
+import com.airsaid.okmock.plugin.model.FieldInfo
 import com.android.build.api.transform.TransformInvocation
+import org.objectweb.asm.*
 
 /**
  * @author airsaid
  */
 class OKMockTransform : AbstractTransform() {
+
+  private val mFieldInfoList = mutableListOf<FieldInfo>()
 
   override fun getName() = OKMockPlugin.PLUGIN_NAME
 
@@ -14,11 +19,20 @@ class OKMockTransform : AbstractTransform() {
 
   }
 
-  override fun onTransform(transformInvocation: TransformInvocation, bytecode: ByteArray): ByteArray {
-    return bytecode
+  override fun onTransform(
+    transformInvocation: TransformInvocation,
+    bytecode: ByteArray
+  ): ByteArray {
+    val cr = ClassReader(bytecode)
+    val cw = ClassWriter(ClassWriter.COMPUTE_FRAMES)
+    val cv = AnnotationFieldClassAdapter(mFieldInfoList, listOf(Mock::class.java), cw)
+    cr.accept(cv, 0)
+    return cw.toByteArray()
   }
 
   override fun onTransformAfter(transformInvocation: TransformInvocation) {
-
+    mFieldInfoList.forEach {
+      println(it)
+    }
   }
 }
