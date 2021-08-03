228ea249d4f648d15fa06d73b8e9cc6e2a384035
Airsaid <airsaid1024@gmail.com>
2021-08-03 15:45:39 +0800

    Update test data

--- app/src/main/java/com/airsaid/okmock/data/Person.kt
+++ app/src/main/java/com/airsaid/okmock/data/Person.kt
@@ -1,6 +1,8 @@
 package com.airsaid.okmock.data
 
+import com.airsaid.okmock.api.MockValue
+
 /**
  * @author airsaid
  */
-data class Person(val id: Long, val name: String, val age: Int)
+data class Person(val id: Long, @MockValue(stringValues = ["小王"]) val name: String, @MockValue(intValues = [18, 22, 30]) val age: Int)
