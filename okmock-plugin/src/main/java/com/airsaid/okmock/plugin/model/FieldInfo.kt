0811914afab70f1fc9d7887e36f7f312ae94f797
Airsaid <airsaid1024@gmail.com>
2021-08-04 11:33:21 +0800

    Add comments for the FieldInfo class

--- okmock-plugin/src/main/java/com/airsaid/okmock/plugin/model/FieldInfo.kt
+++ okmock-plugin/src/main/java/com/airsaid/okmock/plugin/model/FieldInfo.kt
@@ -1,6 +1,21 @@
 package com.airsaid.okmock.plugin.model
 
 /**
+ * Class field information.
+ *
+ * @param owner the internal name of the class in which the field is located.
+ * @param access the field's access flags (see [org.objectweb.asm.Opcodes]). This parameter also indicates if
+ *     the field is synthetic and/or deprecated.
+ * @param name the field's name.
+ * @param descriptor the field's descriptor (see [org.objectweb.asm.Type]).
+ * @param signature the field's signature. May be null if the field's type does not use
+ *     generic types.
+ * @param value the field's value. This parameter, which may be null if the
+ *     field does not have an value.
+ *
  * @author airsaid
  */
-data class FieldInfo(val owner: String, val access: Int, val name: String, val descriptor: String, val signature: String?, val value: Any?)
+data class FieldInfo(
+  val owner: String, val access: Int, val name: String,
+  val descriptor: String, val signature: String?, val value: Any?
+)
