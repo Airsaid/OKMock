f8577065ef9f552265a9b5dfbb686f74a271ba37
Airsaid <airsaid1024@gmail.com>
2021-08-08 22:06:24 +0800

    Add comments for Mock

--- okmock/src/main/java/com/airsaid/okmock/api/Mock.java
+++ okmock/src/main/java/com/airsaid/okmock/api/Mock.java
@@ -6,6 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * This annotation is an identification annotation. that is used to decorate field.
+ * The field to be decorated must not have an assignment operation.
+ * <p>
+ * The decorated fields are automatically populated with random data.
+ *
  * @author airsaid
  */
 @Retention(RetentionPolicy.CLASS)
